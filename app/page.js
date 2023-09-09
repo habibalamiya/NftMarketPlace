99de1443140113050727a308bdbcfb085ad2ab01
unknown <habiba.lamiya@northsouth.edu>
2023-09-09 16:43:28 +0600

    may be there is some mistake

--- app/page.js
+++ app/page.js
@@ -1,53 +1,120 @@
 "use client"
-import { useEffect, useState } from 'react';
-import Header from './components/Header'
-import { ethers } from 'ethers';
-import { ContractAbi } from "./abi/abi.json";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import { ethers } from "ethers";
+import contractabi from "./abi/abi.json"
+const axios = require('axios')
+const FormData = require('form-data')
 
-export default function page(){
-   
-  const[address,setAddress]=useState(null);
-  const[balance,setBalance]=useState(0);
-  const[contract,setContract]=useState(null);
 
-   useEffect(()=>{
 
-     async function initialize(){
-
-      if (typeof window.ethereum !=="undefined"){
-         const provider =new ethers.providers.Web3Provider(window.ethereum);
-         const signer =provider.getSigner();
-         const address=await signer.getAddress();
-         const balance=await provider.getBalance(address);
-        setAddress(address)
-        setBalance(ethers.utils.parseEther(balance))
-        mycontractaddress="0x8c0f149d6965f510910d10d3ab5790fDbCFFC997"
-        const contract =new ethers.Contract( mycontractaddress,abi,signer);
+export default function Home() {
+  const [address, setAddress] = useState(null);
+  const [balance, setBalance] = useState(0);
+  const [contract, setContract] = useState(null);
+  const [name, setName] = useState("");
+  const [description,setDescription] = useState("");
+  const [image, setImage] = useState(null);  
+  useEffect(()=>{
+    async function initialize(){
+      if(typeof window.ethereum !== "undefined"){
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        const balance = await provider.getBalance(address);
+        setAddress(address);
+        // setBalance(ethers.utils.parseEther(balance));
+        const mycontractaddress="0x5f03Dd34391A8eff25eC7C6dB204df476F9798E1";
+        const contract = new ethers.Contract(mycontractaddress,contractabi,signer)
         setContract(contract)
       }
     }
     initialize();
   },[])
-      return (
-        <div>
-        <Header/>
-        <div className="py-80 bg-[url('https://www.womenindigital.net/assets/frontend/img/logo/logo_top.png')] bg-no-repeat bg-center"></div>
-        <div className="text-center">
-        
-        <p className="text-md text-blue-600 lg:text-3xl">hi{address?.slice(0,10)}...{address?.slice(-10)}</p>
+
+  function onChangeFile(e){
+    const file = e.target.files[0];
+    setImage(file);
+    console.log(file)
+  }
+
+ async function onSubmit(event){
+    if(!name && !description && !image){
+      alert("Fill the required details")
+      return;
+    } 
+    const JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJlYjEyN2ZkZi0zZTc2LTQwOWQtYjhiZi0wZDhlMjI1NmRlOTgiLCJlbWFpbCI6ImhhYmliYWxhbWl5YTA2QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJiNTk0Y2JmYzYxYmUzOGZlMTMyMyIsInNjb3BlZEtleVNlY3JldCI6IjUyOTU3YzViODYyNTE2Y2NhZWJiODlhOGY3MGZhMzhhMjgxNDc4ZGQxYTEzMzRiMzAzODFjMzdhMzJjZWNhYTkiLCJpYXQiOjE2OTQyNDQzMjF9.0IglG7aA36GhYtRfLWFS80oxIqwV5Ggj6fFiegLcf90";
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('file', image);
+
+    const pinataMetadata = JSON.stringify({
+      name: 'File name',
+    });
+    formData.append('pinataMetadata', pinataMetadata);
+
+    const pinataOptions = JSON.stringify({
+      cidVersion: 0,
+    });
+    formData.append('pinataOptions', pinataOptions);
+
+    try{
+      const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+          Authorization: "Bearer " + JWT,
+        }
+      });
+      console.log(res.data);
+      const ipfshash=res.data.IpfsHash;
+      
+      const jsondic={
+         name,
+         description,
+         "image":`ipfs/${ipfshash}`
 
 
 
-        </div>
-        
-        
-        
-        
-        
-        
-        
-        </div>
-      )
-
-
+      }
+     
+        const resjson = await axios.post("https://api.pinata.cloud/pinning/pinJSONToIPFS", jsondic, {
+          
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: "Bearer " + JWT,
+          }
+        });
+        const jsonHash=resjson.data.IpfsHash;
+        const tokenURI=`https:://ipfs.io/ipfs/${jsonHash}`;
+        console.log(contract)
+        const conc=contract?.mint(address,tokenURI)
+        console.log("my json hash",resjson.data);
+    } catch (error) {
+      console.error(error);
     }
+
+
+  }
+  return (
+    <>
+    <div>
+   <Header/>
+   <div className="text-center">
+   <p className="text-md text-blue-400 lg:text-3xl">Hi, {address?.slice(0,10)}...{address?.slice(-10)} </p>
+   <div className="flex bg-yellow-400 px-10 mt-5 flex-col space-y-4 py-10 rounded-xl md:mx-[200px] lg:mx-[800px]">
+   <p>NFT MarketPlace</p>
+   <input type="text" placeholder="Enter your name" value={name} onChange={(e)=>{setName(e.target.value)}} className="border border-black px-2 " />
+   <input type="text" placeholder="Enter your description" value={description} onChange={(e)=>{setDescription(e.target.value)}} className="border border-black px-2 " />
+   <div>
+   <label>Upload Image</label>
+   <input type="file" className="mt-2" accept="image/*"  onChange={onChangeFile} />
+   <button className="bg-blue-400 px-4 py-2 rounded-lg mt-4" onClick={onSubmit} >Submit</button>
+   </div>
+   </div>
+  </div>
+    </div>
+    </>
+  )
+}
